7021326cef350f6595ea36512a65f5022e310755
Egor <38540007+NikonorovEgor@users.noreply.github.com>
2022-07-04 10:55:19 +0300

    Initial commit

---
+++ js/aircraft feedback.js
@@ -0,0 +1,360 @@
+window.onload = function () {
+    document.getElementById("turs-container").style.display = "none";
+}
+
+$(document).ready(function () {
+    $("a.robinsonR44_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#robinsonR44")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#robinsonR44").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.robinsonR66_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#robinsonR66")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#robinsonR66").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.eurocopterAS350_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#eurocopterAS350")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#eurocopterAS350").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.eurocopterAS355_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#eurocopterAS355")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#eurocopterAS355").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.eurocopterEC120_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#eurocopterEC120")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#eurocopterEC120").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.eurocopterEC130_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#eurocopterEC130")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#eurocopterEC130").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.augustaAW109_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#augustaAW109")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#augustaAW109").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.augustaAW119_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#augustaAW119")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#augustaAW119").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.augustaAW139_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#augustaAW139")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#augustaAW139").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.bell407_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#bell407")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#bell407").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.bell429_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#bell429")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#bell429").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.bell505_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#bell505")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#bell505").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.mi8_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#mi8")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#mi8").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.premier1A_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#premier1A")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#premier1A").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.kingAirC90_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#kingAirC90")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#kingAirC90").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.pc12_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#pc12")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#pc12").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.piperPA_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#piperPA")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#piperPA").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.hawker800_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#hawker800")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#hawker800").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("a.embraer600_btn").click(function (event) {
+        event.preventDefault();
+        $("#myOverlay").fadeIn(297, function () {
+            $("#embraer600")
+                .css("display", "block")
+                .animate({ opacity: 1 }, 198);
+        });
+    });
+
+    $("#myModal__close, #myOverlay").click(function () {
+        $("#embraer600").animate({ opacity: 0 }, 198, function () {
+            $(this).css("display", "none");
+            $("#myOverlay").fadeOut(297);
+        });
+    });
+});
+// ----------------------------------------------------------------------------
+$(document).ready(function () {
+    $("#zakazat-tur").click(function (event) {
+        event.preventDefault();
+        $("#turs-container").css("display", "block").css("opacity", "0.2").animate({opacity: 1}, 1000);
+        $("#vybrat-tur").css("display","none").animate({opacity: 0.2}, 1000);
+        $("html, body").animate({ scrollTop: $('#turs-container').offset().top }, 400);
+    })
+})
+// var a = document.getElementById("zakazat-tur");
+// a.onclick = function () {
+//     document.getElementById("turs-container").style.display = "block";
+//     document.getElementById("vybrat-tur").style.display = "none";
+//     return false;
+// }
